8c1bbf375ce39110112ca5dc43607272dae55686
emma11y <emmanuelle.aboaf@dcube.fr>
2021-12-09 10:36:50 +0100

    bootstrap

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -7,11 +7,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { TtsMozillaComponent } from './components/tts-mozilla/tts-mozilla.component';
 
-const angularModules = [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, HttpClientModule, NgbToastModule];
+const angularModules = [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, HttpClientModule, NgbModule, NgbToastModule];
 
 const components = [AlertComponent, HeaderComponent, TtsMozillaComponent];
 
